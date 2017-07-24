6449ab7f92c909f6f25a3ba08daa396b5dc5be93
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-07-24 12:41:10 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './browser/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js' // assumes your bundle.js will also be in the root of your project folder
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
